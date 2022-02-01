e8a922cdd1e90ac86f9680f16289d7c898c94706
YeJun <hayejun1013@naver.com>
2022-02-01 18:01:35 +0900

    add: add board validation pipes

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,25 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../boards.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
+  transform(value: any) {
+    if (typeof value !== 'string') {
+      throw new BadRequestException(`${value} should be string argument`);
+    }
+
+    value = value.toUpperCase();
+
+    if (!this.isValidStatus(value)) {
+      throw new BadRequestException(`${value} isn't in the status options`);
+    }
+
+    return value;
+  }
+
+  private isValidStatus(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+}
