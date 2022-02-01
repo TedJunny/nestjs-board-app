0dd58c2b6e56712acab2ae88ff82197bcfc5adfe
YeJun <hayejun1013@naver.com>
2022-02-01 15:50:36 +0900

    add: add board CRUD function

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { Board } from './boards.model';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Board, BoardStatus } from './boards.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -9,5 +18,28 @@ export class BoardsController {
   @Get()
   getAllBoard(): Board[] {
     return this.boardService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardService.createBoard(createBoardDto);
+  }
+
+  @Get(':id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardService.getBoardById(id);
+  }
+
+  @Delete(':id')
+  deleteBoard(@Param('id') id: string): void {
+    return this.boardService.deleteBoard(id);
+  }
+
+  @Patch(':id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus,
+  ): Board {
+    return this.boardService.updateBoardStatus(id, status);
   }
 }
