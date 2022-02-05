431f6bd822972c52d4b98a188eeefed3b92cc980
YeJun <hayejun1013@naver.com>
2022-02-06 05:18:20 +0900

    add: add board CRUD APIs using typeorm functions

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -4,12 +4,14 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
@@ -19,31 +21,31 @@ export class BoardsController {
   constructor(private boardService: BoardsService) {}
 
   @Get()
-  getAllBoard(): Board[] {
+  getAllBoards(): Promise<Board[]> {
     return this.boardService.getAllBoards();
   }
 
   @Post()
   @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
     return this.boardService.createBoard(createBoardDto);
   }
 
   @Get(':id')
-  getBoardById(@Param('id') id: string): Board {
+  getBoardById(@Param('id') id: number): Promise<Board> {
     return this.boardService.getBoardById(id);
   }
 
   @Delete(':id')
-  deleteBoard(@Param('id') id: string): void {
+  deleteBoard(@Param('id', ParseIntPipe) id): Promise<void> {
     return this.boardService.deleteBoard(id);
   }
 
   @Patch(':id/status')
   updateBoardStatus(
-    @Param('id') id: string,
+    @Param('id', ParseIntPipe) id: number,
     @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  ): Board {
+  ) {
     return this.boardService.updateBoardStatus(id, status);
   }
 }
